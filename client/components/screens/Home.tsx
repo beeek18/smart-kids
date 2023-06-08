9c7bfea3cf1c2c6652975a28caa7b9f2b5aae126
Alena <alena58@mail.ru>
2023-06-08 18:02:20 +0300

    sound, info, avatarka

--- client/components/screens/Home.tsx
+++ client/components/screens/Home.tsx
@@ -1,17 +1,29 @@
 import { Button, View } from 'react-native';
 import { Avatar } from '@rneui/themed';
 import { Text } from 'react-native-elements';
+import { useState } from 'react';
 
 export default function Home({ navigation }): JSX.Element {
+  const [sound, setSound] = useState(false);
+  const onClick = () => {
+    setSound((prev) => !prev);
+  };
   return (
     <View>
       <Avatar size={100} rounded title="😎" />
-      <Text h1>🔊</Text>
-      <Text h1>🔇</Text>
-      <Text h1>ℹ️</Text>
+      {sound !== true && (
+        <Text onPress={() => onClick()} h1>
+          🔊
+        </Text>
+      )}
+      {sound === true && (
+        <Text onPress={() => onClick()} h1>
+          🔇
+        </Text>
+      )}
       <Button onPress={() => navigation.navigate('Categories')} title="Начать игру" />
       <Button onPress={() => navigation.navigate('Profile')} title="Profile" />
-      <Button onPress={() => navigation.navigate('Info')} title="Info" />
+      <Button onPress={() => navigation.navigate('Info')} title="ℹ️" />
     </View>
   );
 }
