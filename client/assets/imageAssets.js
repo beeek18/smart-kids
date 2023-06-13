b1a9590604ec62d869dab0f0951d3fa45e5ece7b
Alena <alena58@mail.ru>
2023-06-13 18:00:47 +0300

    done css profilePage

--- client/assets/imageAssets.js
+++ client/assets/imageAssets.js
@@ -3,5 +3,6 @@ export const ImagesAssets = {
   avatar2: require('./icons/avatar2.png'),
   avatar3: require('./icons/avatar3.png'),
   avatar4: require('./icons/avatar4.png'),
+  avatar5: require('./icons/avatar5.png'),
   ///you can add more many images like this here.
 };
