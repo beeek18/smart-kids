64c825daa602516544abefc0710b1535e35a4a61
Bekzodbek <kodirov2000@list.ru>
2023-06-15 16:47:54 +0300

    fix types in slices

--- client/features/clickSound.js
+++ client/features/clickSound.js
@@ -4,11 +4,9 @@ async function clickSound() {
   try {
     const { sound } = await Audio.Sound.createAsync(require('../assets/click.wav'));
     await sound.playAsync();
-    // console.log('Sound loaded', sound);
   } catch (error) {
     console.log(error);
   }
 }
-
 
 export default clickSound;
